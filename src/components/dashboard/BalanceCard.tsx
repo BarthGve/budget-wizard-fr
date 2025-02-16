0313b117c9f7de876503a5bfdac3c9b4f61f9e2d
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-16 12:37:52 +0100

    test

--- src/components/dashboard/BalanceCard.tsx
+++ src/components/dashboard/BalanceCard.tsx
@@ -18,8 +18,8 @@ export const BalanceCard = ({
       </CardHeader> */}
       
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Solde</CardTitle>
-          <Scale className="h-4 w-4 text-muted-foreground" />
+          <CardTitle className="text-2xl">Solde</CardTitle>
+          <Scale className="h-6 w-6 text-muted-foreground" />
         </CardHeader>
       
       
