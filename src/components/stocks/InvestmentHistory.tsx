ff0fe9060d5fd465638dc333372b9fa539f2bb3c
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-16 23:27:56 +0100

    fix test

--- src/components/stocks/InvestmentHistory.tsx
+++ src/components/stocks/InvestmentHistory.tsx
@@ -29,7 +29,7 @@ export const InvestmentHistory = ({ data }: InvestmentHistoryProps) => {
                 tickLine={false}
                 axisLine={false}
                 tickMargin={8}
-                tickFormatter={(value) => value.slice(0, 3)} />
+               />
           
               <Tooltip 
                 formatter={(value) => 
