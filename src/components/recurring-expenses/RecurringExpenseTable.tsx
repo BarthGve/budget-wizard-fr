5314deca0ba40fd9b5b3601209f2538533abc79e
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-18 00:07:52 +0100

    test

--- src/components/recurring-expenses/RecurringExpenseTable.tsx
+++ src/components/recurring-expenses/RecurringExpenseTable.tsx
@@ -96,7 +96,7 @@ export const RecurringExpenseTable = ({ expenses, onDeleteExpense }: RecurringEx
                         }}
                       />
                     )}
-                    <span>{expense.name}</span>
+                    <span className="text-semibold">{expense.name}</span>
                   </div>
                 </TableCell>
                 <TableCell className="border-t border-b  border-slate-200  py-2">{expense.category}</TableCell>
