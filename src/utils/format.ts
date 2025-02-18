0542a1375d6e8c0c3337df37d043d46959edc7f4
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-18 06:48:32 +0100

    test

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,5 +3,7 @@ export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat("fr-FR", {
     style: "currency",
     currency: "EUR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
   }).format(amount);
 };
