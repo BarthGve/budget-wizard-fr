4be6c27ec7844df8e54330f9795abe0cdc50dbcc
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-19 06:31:38 +0100

    test bouton

--- src/components/contributors/AddContributorDialog.tsx
+++ src/components/contributors/AddContributorDialog.tsx
@@ -34,7 +34,7 @@ export const AddContributorDialog = ({ onAdd }: AddContributorDialogProps) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="bg-primary text-primary-foreground hover:bg-primary-hover">
+        <Button className="text-primary-foreground bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 shadow-md ">
           <UserPlus className="mr-2 h-4 w-4" />
           Ajouter un contributeur
         </Button>
