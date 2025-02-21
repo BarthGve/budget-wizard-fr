35bbee07e67943440c5c2e27efa6a0ff42a71f86
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-21 23:08:48 +0100

    test

--- src/components/recurring-expenses/RecurringExpenseTable.tsx
+++ src/components/recurring-expenses/RecurringExpenseTable.tsx
@@ -73,8 +73,7 @@ export const RecurringExpenseTable = ({ expenses, onDeleteExpense }: RecurringEx
               <SelectValue placeholder="Lignes par page"/>
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="5">5 lignes</SelectItem>
-              <SelectItem value="15">15 lignes</SelectItem>
+              <SelectItem value="10">10 lignes</SelectItem>
               <SelectItem value="25">25 lignes</SelectItem>
               <SelectItem value="-1">Toutes les lignes</SelectItem>
             </SelectContent>
