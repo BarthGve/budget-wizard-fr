4adf0ef3701f1c23194301a41f2a1b0c7e7e664f
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-22 10:16:57 +0100

    test

--- src/components/settings/expense-categories/CategoryForm.tsx
+++ src/components/settings/expense-categories/CategoryForm.tsx
@@ -22,9 +22,8 @@ export const CategoryForm = () => {
         onChange={(e) => setNewCategory(e.target.value)}
         onKeyPress={(e) => e.key === "Enter" && handleAddCategory()}
       />
-      <Button onClick={handleAddCategory} disabled={isAdding}>
-        <Plus className="h-4 w-4 mr-2" />
-        Ajouter
+      <Button onClick={handleAddCategory} disabled={isAdding} className="text-primary-foreground h-8 w-8 p-0 rounded-full bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 shadow-md">
+        <Plus className="h-4 w-4 " />
       </Button>
     </div>
   );
