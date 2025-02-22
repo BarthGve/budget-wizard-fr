5a5fc0124975b3f957e2c1eafc5432b9225b505d
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-22 10:04:26 +0100

    test

--- src/components/settings/RetailersSettings.tsx
+++ src/components/settings/RetailersSettings.tsx
@@ -22,8 +22,7 @@ export const RetailersSettings = () => {
             onOpenChange={setOpen}
             trigger={
               <Button className="text-primary-foreground rounded-full bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 shadow-md">
-                <Plus className="mr-2 h-4 w-4" />
-               
+                <Plus className="h-4 w-4" />
               </Button>
             }
             onRetailerSaved={() => {
