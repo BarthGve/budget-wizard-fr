4f6c89eb5e041ccf6c4674d5d12e21889329547f
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-02-24 23:38:25 +0100

    test

--- supabase/functions/send-reset-password/index.ts
+++ supabase/functions/send-reset-password/index.ts
@@ -34,8 +34,8 @@ const handler = async (req: Request): Promise<Response> => {
       to: [email],
       subject: "Réinitialisation de votre mot de passe",
       html: `
-      <!DOCTYPE html>
-<html lang="fr">
+    
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -105,7 +105,7 @@ const handler = async (req: Request): Promise<Response> => {
 </div>
 
 </body>
-</html>
+
       `,
     });
 
