160ac065ea0e81dc3c6b9d913c96b41b1fb0dce4
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-03-02 01:26:56 +0100

    settings categories form

--- src/components/settings/expense-categories/CategoryForm.tsx
+++ src/components/settings/expense-categories/CategoryForm.tsx
@@ -15,17 +15,24 @@ export const CategoryForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-between space-x-2">
-      <Input
-       className="w-64" 
-        placeholder="Nouvelle catégorie"
-        value={newCategory}
-        onChange={(e) => setNewCategory(e.target.value)}
-        onKeyPress={(e) => e.key === "Enter" && handleAddCategory()}
-      />
-      <Button onClick={handleAddCategory} disabled={isAdding} className="flex items-center justify-center text-primary-foreground h-8 w-8 p-0 rounded-full bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 shadow-md">
-        <Plus className="h-4 w-4 " />
-      </Button>
-    </div>
+    <div className="flex flex-row items-center space-x-2">
+  <div className="flex-grow">
+    <Input
+      placeholder="Nouvelle catégorie"
+      value={newCategory}
+      onChange={(e) => setNewCategory(e.target.value)}
+      onKeyPress={(e) => e.key === "Enter" && handleAddCategory()}
+    />
+  </div>
+  <div className="flex-shrink-0">
+    <Button 
+      onClick={handleAddCategory} 
+      disabled={isAdding} 
+      className="flex items-center justify-center text-primary-foreground h-8 w-8 p-0 rounded-full bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 shadow-md"
+    >
+      <Plus className="h-4 w-4" />
+    </Button>
+  </div>
+</div>
   );
 };
