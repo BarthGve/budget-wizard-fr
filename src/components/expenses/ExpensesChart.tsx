abfc7ad74fcfeb54a7fa6bef454b297b9aadccb4
Bruno <66181324+BarthGve@users.noreply.github.com>
2025-03-04 23:00:15 +0100

    test

--- src/components/expenses/ExpensesChart.tsx
+++ src/components/expenses/ExpensesChart.tsx
@@ -81,7 +81,7 @@ export function ExpensesChart({ expenses, viewMode }: ExpensesChartProps) {
 
   return (
     <div className="bg-card rounded-lg p-2 mt-2">
-      <ChartContainer config={chartConfig} className="h-[300p]">
+      <ChartContainer config={chartConfig} >
         <ResponsiveContainer width="100%" height={75}>
           <BarChart data={chartData} margin={{ top: 0, right: 0, left: 0, bottom: 20 }}>
             <CartesianGrid vertical={false} stroke="hsl(var(--border))" opacity={0.1} />
